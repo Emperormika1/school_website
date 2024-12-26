0fd1118c2247686d03b6a1dad93981c7a683c549
Emperormika1 <sj029866@sarasit.ac.th>
2024-12-26 16:13:42 +0700

    ‘newest

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./views/*.ejs"
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '128': '32rem',
+      }
+    },
   },
   plugins: [],
   screens: {
